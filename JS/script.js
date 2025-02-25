90b35bbd7fcbdb80c1eb49f6a8fb8b73687429f2
lostkittxn <126134611+lostkittxn@users.noreply.github.com>
2025-02-25 15:52:46 -0600

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,111 +1,82 @@
 function moveToSelected(element) {
+    if (element == "next") {
+        var selected = $(".selected").next();
+    } else if (element == "prev") {
+        var selected = $(".selected").prev();
+    } else {
+        var selected = element;
+    }
 
-  if (element == "next") {
-    var selected = $(".selected").next();
-  } else if (element == "prev") {
-    var selected = $(".selected").prev();
-  } else {
-    var selected = element;
-  }
+    var next = $(selected).next();
+    var prev = $(selected).prev();
+    var prevSecond = $(prev).prev();
+    var nextSecond = $(next).next();
 
-  var next = $(selected).next();
-  var prev = $(selected).prev();
-  var prevSecond = $(prev).prev();
-  var nextSecond = $(next).next();
-
-  $(selected).removeClass().addClass("selected");
-
-  $(prev).removeClass().addClass("prev");
-  $(next).removeClass().addClass("next");
-
-  $(nextSecond).removeClass().addClass("nextRightSecond");
-  $(prevSecond).removeClass().addClass("prevLeftSecond");
-
-  $(nextSecond).nextAll().removeClass().addClass('hideRight');
-  $(prevSecond).prevAll().removeClass().addClass('hideLeft');
-
+    $(selected).removeClass().addClass("selected");
+    $(prev).removeClass().addClass("prev");
+    $(next).removeClass().addClass("next");
+    $(nextSecond).removeClass().addClass("nextRightSecond");
+    $(prevSecond).removeClass().addClass("prevLeftSecond");
+    $(nextSecond).nextAll().removeClass().addClass('hideRight');
+    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
 }
 
 // Eventos teclado
 $(document).keydown(function(e) {
     switch(e.which) {
         case 37: // left
-        moveToSelected('prev');
-        break;
-
+            moveToSelected('prev');
+            break;
         case 39: // right
-        moveToSelected('next');
-        break;
-
+            moveToSelected('next');
+            break;
         default: return;
     }
     e.preventDefault();
 });
 
+// Manejo de clics en los elementos del carrusel
 $('#carousel div').click(function() {
-  moveToSelected($(this));
+    moveToSelected($(this));
 });
 
 $('#prev').click(function() {
-  moveToSelected('prev');
+    moveToSelected('prev');
 });
 
 $('#next').click(function() {
-  moveToSelected('next');
+    moveToSelected('next');
 });
 
-function moveToSelected(element) {
-
-  if (element == "next") {
-    var selected = $(".selected").next();
-  } else if (element == "prev") {
-    var selected = $(".selected").prev();
-  } else {
-    var selected = element;
-  }
-
-  var next = $(selected).next();
-  var prev = $(selected).prev();
-  var prevSecond = $(prev).prev();
-  var nextSecond = $(next).next();
-
-  $(selected).removeClass().addClass("selected");
-
-  $(prev).removeClass().addClass("prev");
-  $(next).removeClass().addClass("next");
-
-  $(nextSecond).removeClass().addClass("nextRightSecond");
-  $(prevSecond).removeClass().addClass("prevLeftSecond");
-
-  $(nextSecond).nextAll().removeClass().addClass('hideRight');
-  $(prevSecond).prevAll().removeClass().addClass('hideLeft');
-
+// Función para manejar el deslizamiento
+function handleSwipe(direction) {
+    if (direction === 'left') {
+        moveToSelected('prev');
+    } else if (direction === 'right') {
+        moveToSelected('next');
+    }
 }
 
-// Eventos teclado
-$(document).keydown(function(e) {
-    switch(e.which) {
-        case 37: // left
-        moveToSelected('prev');
-        break;
+// Detectar el deslizamiento
+let touchstartX = 0;
+let touchendX = 0;
 
-        case 39: // right
-        moveToSelected('next');
-        break;
+const carousel = document.getElementById('carousel'); // Asegúrate de que el ID sea correcto
 
-        default: return;
+carousel.addEventListener('touchstart', e => {
+    touchstartX = e.changedTouches[0].screenX;
+});
+
+carousel.addEventListener('touchend', e => {
+    touchendX = e.changedTouches[0].screenX;
+    handleGesture();
+});
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        handleSwipe('left');
     }
-    e.preventDefault();
-});
-
-$('#carousel2 div').click(function() {
-  moveToSelected($(this));
-});
-
-$('#prev').click(function() {
-  moveToSelected('prev');
-});
-
-$('#next').click(function() {
-  moveToSelected('next');
-});
+    if (touchendX > touchstartX) {
+        handleSwipe('right');
+    }
+}
